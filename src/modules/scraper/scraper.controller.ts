4545b83f89696b670f18b158beed84924af7c15f
Ogeday Oztoprak <ogeday.oztoprak@foreside.nl>
2023-10-15 15:01:16 +0200

    Improvements have been made

--- src/modules/scraper/scraper.controller.ts
+++ src/modules/scraper/scraper.controller.ts
@@ -16,25 +16,6 @@ export class ScraperController {
     // Your scraper logic here
   }
 
-  @Get('test')
-  async test() {
-    try {
-      const twitterClient = new TwitterApi({
-        appKey: this.configService.get('twitter.consumerKey'),
-        appSecret: this.configService.get('twitter.consumerSecret'),
-        accessToken: this.configService.get('twitter.accessTokenKey'), // oauth token from previous step (link generation)
-        accessSecret: this.configService.get('twitter.accessTokenSecret'), // oauth token secret from previous step (link generation)
-      });
-      const response = await twitterClient.v2.tweet('Hello World!');
-      console.log(response);
-      const ibo = await twitterClient.v2.userByUsername('haskaloglu');
-      console.log(ibo);
-      return ibo;
-    } catch (error) {
-      console.error('Failed to tweet:', error);
-    }
-  }
-
   @Get('isPolitics')
   async isPolitics() {
     try {
