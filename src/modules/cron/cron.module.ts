2b8b48560229da0bdc4869e2c245e3057db94c02
Ogeday Oztoprak <ogeday.oztoprak@foreside.nl>
2023-10-21 18:05:14 +0200

    Hot fix

--- src/modules/cron/cron.module.ts
+++ src/modules/cron/cron.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { CronService } from './cron.service';
-import { TweeterService } from '../tweeter/tweeter.service';
 import { CronController } from './cron.controller';
+import { ScraperService } from '../scraper/scraper.service';
+import { TweeterService } from '../tweeter/tweeter.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { News, NewsSchema } from '../../schemas/news';
 
 @Module({
-  providers: [CronService, TweeterService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: News.name,
+        schema: NewsSchema,
+      },
+    ]),
+  ],
+  providers: [CronService, ScraperService, TweeterService],
   controllers: [CronController],
 })
 export class CronModule {}
